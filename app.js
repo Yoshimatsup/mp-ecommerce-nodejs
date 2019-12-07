e957f219ab2ffa71b057bb08e6c8fcb8acb8a1eb
Junior <daniel.y.junior@accenture.com>
2019-12-07 15:31:30 -0300

    feature(integrating form with mpago api)

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+require('dotenv').config();
 var express = require('express');
 var exphbs  = require('express-handlebars');
+var bodyParser = require('body-parser')
 var port = process.env.PORT || 3000
 
+var mp = require('mercadopago')
 var app = express();
- 
+
+const public_token='TEST-560060d2-6043-4fd3-b752-6e74586afa3f'
+const secret_token='TEST-3300541393630167-120714-97fbcca39e84ccfc893c2e93bafed1a5-270969919'
+
+app.use(bodyParser.urlencoded({ extended: true }))
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
-
 app.use(express.static('assets'));
  
 app.use('/assets', express.static(__dirname + '/assets'));
@@ -15,7 +21,13 @@ app.get('/', function (req, res) {
     res.render('home');
 });
 
+app.post('/payment_processing', function (req, res){
+    console.log(req.body);
+    res.send();
+})
+
 app.get('/detail', function (req, res) {
+    console.log(mp)
     res.render('detail', req.query);
 });
 
---
+++ controllers/paymentController.js
@@ -0,0 +1,2 @@
+var mp = require('mercadopago')
+
